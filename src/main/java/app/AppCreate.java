d164b4e65e1b34ee11f11d8b3d26bbdc9226364c
superior <carolvitoria1705@gmail.com>
2021-12-02 21:58:26 -0300

    now now

--- src/main/java/app/AppCreate.java
+++ src/main/java/app/AppCreate.java
@@ -5,9 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import connection.ConnectionFactory;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
